0823c009020248b242e98ba5f546b2e9285f8060
Asuu <suqinzheng_0624@163.com>
2019-12-25 11:40:31 +0800

    [zhengsuqin] feat: complete the function to display diff task when click diff button

--- static/script/index.js
+++ static/script/index.js
@@ -1,7 +1,7 @@
 var taskInput = document.getElementById("task-input");
 var taskList = document.getElementsByTagName("ol")[0];
 var todoList = document.getElementsByClassName("todo-list")[0];
-
+var taskStatus = "all";
 taskInput.addEventListener("keydown", function(event) {
 	if (event.keyCode === 13) {
 		addStorage();
@@ -10,12 +10,25 @@ taskInput.addEventListener("keydown", function(event) {
 
 todoList.addEventListener("click", function(event) {
 	var eventTarget = event.target;
-	switch (eventTarget.className) {
+
+	switch (eventTarget.name) {
 		case "add-btn":
 			addStorage();
 			break;
 		case "hasDone":
-			changeStyle(eventTarget);
+			changeStatus(eventTarget);
+			break;
+		case "all":
+			taskStatus = "all";
+			showTask(taskStatus);
+			break;
+		case "to-do":
+			taskStatus = "todo";
+			showTask(taskStatus);
+			break;
+		case "done":
+			taskStatus = "done";
+			showTask(taskStatus);
 			break;
 		default:
 			break;
@@ -23,21 +36,34 @@ todoList.addEventListener("click", function(event) {
 });
 
 function addStorage() {
-	var task = taskInput.value;
+	var task = taskInput.value.trim();
 	if (task === "") {
 		return;
 	}
 	if (!localStorage.getItem(task)) {
-		var localTask = [localStorage.length, task, "", true];
-		localStorage.setItem(task, JSON.stringify(localTask));
-		addTask(task);
+		var localValue = [localStorage.length, task, ""];
+		localStorage.setItem(task, JSON.stringify(localValue));
+		if (taskStatus !== "done") {
+			addTask(task);
+		}
 	}
 	taskInput.value = "";
 }
 
 function addTask(item) {
-	taskList.innerHTML += `<li><input class="hasDone" type="checkbox" name="complete-task" />
-  <span>${item}</span></li>`;
+	var task = JSON.parse(localStorage.getItem(item));
+	var hasChecked = task[2];
+	taskList.innerHTML += `<li><input type="checkbox" name="hasDone" ${hasChecked}/>
+  <span>${task[1]}</span></li>`;
+}
+
+function changeStatus(item) {
+	var status = item.checked;
+	var content = item.parentNode.innerText;
+	var index = JSON.parse(localStorage.getItem(content));
+	var localTask = [index, item.parentNode.innerText, status, true];
+	localStorage.setItem(content, JSON.stringify(localTask));
+	changeStyle(item);
 }
 
 function changeStyle(item) {
@@ -45,7 +71,26 @@ function changeStyle(item) {
 		item.parentNode.style.color = "#999999";
 		item.parentNode.style.textDecorationLine = "line-through";
 	} else {
-    item.parentNode.style.color = "black";
+		item.parentNode.style.color = "black";
 		item.parentNode.style.textDecorationLine = "";
-  }
+	}
+}
+
+function showTask(status) {
+	var taskItem = document.getElementsByTagName("li");
+	for (var li of taskItem) {
+		switch (status) {
+			case "all":
+				li.hidden = false;
+				break;
+			case "todo":
+				li.hidden = li.firstChild.checked;
+				break;
+			case "done":
+				li.hidden = !li.firstChild.checked;
+				break;
+			default:
+				break;
+		}
+	}
 }
